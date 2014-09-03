aa0485317249477b41748e60af2a2c3d9a61bf1e
Mathew Grabau <mgrabau@mathewgrabau.ca>
2014-09-03 12:11:57 -0700

    Updated all of the node_modules

--- node_modules/d3/src/math/trigonometry.js
+++ node_modules/d3/src/math/trigonometry.js
@@ -10,6 +10,14 @@ function d3_sgn(x) {
   return x > 0 ? 1 : x < 0 ? -1 : 0;
 }
 
+// Returns the 2D cross product of AB and AC vectors, i.e., the z-component of
+// the 3D cross product in a quadrant I Cartesian coordinate system (+x is
+// right, +y is up). Returns a positive value if ABC is counter-clockwise,
+// negative if clockwise, and zero if the points are collinear.
+function d3_cross2d(a, b, c) {
+  return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]);
+}
+
 function d3_acos(x) {
   return x > 1 ? 0 : x < -1 ? π : Math.acos(x);
 }
