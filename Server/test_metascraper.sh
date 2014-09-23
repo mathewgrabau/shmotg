e6db4ca4b4311b11a497b9d9f67f6551925290e6
Mathew Grabau <mgrabau@mathewgrabau.ca>
2014-09-23 10:14:35 -0700

    Added two testing scripts for local testing.

---
+++ Server/test_metascraper.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+node metascraper.js 2014 7 1 2014 7 2	1
+# NOTE: something interesting happened when I put January as start and July as the end, investigate that.
---
+++ Server/test_scraper.sh
@@ -0,0 +1,7 @@
+#scraper.js loaded
+#USAGE:
+#  start_year(YYYY) start_month(1-12) start_day(1-31) start_hour(0-23)
+#  end_year(YYYY) end_month(1-12) end_day(1-31) end_hour(0-23)
+#  sensorNumber
+
+node scraper.js 2014 7 1 0 2014 7 1 6 1
